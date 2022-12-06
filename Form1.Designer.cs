351d70c804713edc6d60b032b25735fed358de35
Gabriel de Almeida <work.gabriel.as@gmail.comgit config --global user.email work.gabriel.as@gmail.com>
2022-12-05 22:25:06 -0300

    Adicionados mais componentes no FORM

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -37,6 +37,10 @@ namespace CRUD_C_SHARP
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // Nome
@@ -44,7 +48,7 @@ namespace CRUD_C_SHARP
             this.Nome.AutoSize = true;
             this.Nome.BackColor = System.Drawing.SystemColors.Control;
             this.Nome.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.Nome.Location = new System.Drawing.Point(81, 19);
+            this.Nome.Location = new System.Drawing.Point(12, 16);
             this.Nome.Name = "Nome";
             this.Nome.Size = new System.Drawing.Size(41, 16);
             this.Nome.TabIndex = 0;
@@ -54,7 +58,7 @@ namespace CRUD_C_SHARP
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(81, 53);
+            this.label1.Location = new System.Drawing.Point(12, 44);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(54, 16);
             this.label1.TabIndex = 1;
@@ -62,14 +66,14 @@ namespace CRUD_C_SHARP
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(145, 16);
+            this.textBox1.Location = new System.Drawing.Point(109, 12);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.Size = new System.Drawing.Size(433, 22);
             this.textBox1.TabIndex = 2;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(395, 45);
+            this.button1.Location = new System.Drawing.Point(691, 19);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 3;
@@ -78,37 +82,70 @@ namespace CRUD_C_SHARP
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(145, 50);
+            this.textBox2.Location = new System.Drawing.Point(109, 41);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.Size = new System.Drawing.Size(172, 22);
             this.textBox2.TabIndex = 4;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(395, 74);
+            this.button2.Location = new System.Drawing.Point(107, 103);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(87, 23);
             this.button2.TabIndex = 5;
             this.button2.Text = "Editar";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(395, 103);
+            this.button3.Location = new System.Drawing.Point(200, 103);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.Size = new System.Drawing.Size(81, 23);
             this.button3.TabIndex = 6;
             this.button3.Text = "Excluir";
             this.button3.UseVisualStyleBackColor = true;
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(395, 16);
+            this.button4.Location = new System.Drawing.Point(12, 103);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.Size = new System.Drawing.Size(88, 23);
             this.button4.TabIndex = 7;
             this.button4.Text = "Incluir";
             this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(691, 50);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 22);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "Sair";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Window;
+            this.panel1.Location = new System.Drawing.Point(12, 141);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(776, 327);
+            this.panel1.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 16);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "CPF/CNPJ";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(109, 69);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(172, 22);
+            this.textBox3.TabIndex = 11;
             // 
             // Form1
             // 
@@ -116,6 +153,10 @@ namespace CRUD_C_SHARP
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
             this.ClientSize = new System.Drawing.Size(800, 480);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
@@ -143,6 +184,10 @@ namespace CRUD_C_SHARP
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
     }
 }
 
